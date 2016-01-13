61cfa1c9b8d674e9de9e79cd51eef185fdcf0d8d
Blair Peterson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-13 13:37:19 -0800

    add a loop to js

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   // array of flavors
   var flavors = ["vanilla","chocolate","peach","strawberry"];
   // loop through flavors
-
+  flavors.forEach(function(flavor) {
+    alert("I'm working " + flavor);
+  });
   // add flavors to list
 });
